16382680c61846548f4098c5cfd17dae95cb0acb
juzybits <96890594+juzybits@users.noreply.github.com>
2023-08-13 04:23:02 +0000

    move React stuff to @polymedia/react-components

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ export * from './addresses';
 export * from './links';
 export * from './makeCssUrl';
 export * from './network';
-export * from './NetworkSelector';
 export * from './rpcConfig';
-export * from './useClickOutside';
