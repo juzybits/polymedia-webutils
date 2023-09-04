89d78fae8504e6ea978a071d87aa14de9535a7e3
juzybits <96890594+juzybits@users.noreply.github.com>
2023-09-04 05:23:08 +0000

    update RPC config: mainnet_fullnode comostation

--- scripts/test_endpoints.sh
+++ scripts/test_endpoints.sh
@@ -15,25 +15,11 @@ while getopts 'c' option; do
 done
 
 SUI_RPC_HOSTS=(
-  # Testnet
-  # 'https://fullnode.testnet.sui.io'                 # new jersey
-  # 'https://rpc-testnet.suiscan.xyz'                 # california
-  # 'https://sui-testnet.nodeinfra.com'               # arizona
-  # 'https://fullnode.testnet.vincagame.com'          # germany
-  # 'https://sui-rpc.testnet.lgns.net'                # lithuania | obsolete data
-  # 'https://testnet.artifact.systems/sui'            # new jersey | The response returned from RPC server does not match the TypeScript definition. This is likely because the SDK version is not compatible with the RPC server.
-  # 'https://sui-testnet-rpc.allthatnode.com'         # germany | 429 when fetching profiles
-  # 'https://sui-testnet-rpc-germany.allthatnode.com' # germany | 429 when fetching profiles
-  # 'https://sui-testnet-endpoint.blockvision.org'    # singapore | 1.8s
-  # 'https://sui-testnet-rpc-korea.allthatnode.com'   # korea | 2.4s
-  # 'https://sui-testnet-fullnode.quantnode.tech'     # germany | Fast, but CORS error
-  # 'https://sui-rpc-pt.testnet-pride.com'            # france | 502
-
   # Mainnet
   'https://fullnode.mainnet.sui.io'
   'https://rpc-mainnet.suiscan.xyz'
-  'https://sui-mainnet.nodeinfra.com'
-  'https://sui-mainnet-rpc.bartestnet.com'
+  # 'https://sui-mainnet.nodeinfra.com'               # 429
+  # 'https://sui-mainnet-rpc.bartestnet.com'          # 502
   'https://mainnet.sui.rpcpool.com'
   'https://mainnet-rpc.sui.chainbase.online'
   'https://sui-mainnet.public.blastapi.io'
@@ -47,11 +33,11 @@ SUI_RPC_HOSTS=(
   'https://sui-mainnet-rpc.allthatnode.com'
   'https://sui-mainnet-us-1.cosmostation.io'
   'https://sui-mainnet-us-2.cosmostation.io'
-  'https://sui-mainnet.blockeden.xyz'
+  # 'https://sui-mainnet.blockeden.xyz'               # 000
   'https://sui-rpc-mainnet.testnet-pride.com'
   'https://sui1mainnet-rpc.chainode.tech'
-  'https://sui-mainnet-endpoint.blockvision.org'
-  'https://sui-mainnet-rpc-korea.allthatnode.com'
+  # 'https://sui-mainnet-endpoint.blockvision.org'    # 404
+  # 'https://sui-mainnet-rpc-korea.allthatnode.com'   # slow
   # 'https://sui.publicnode.com' # 403 forbidden
   # 'wss://mainnet.sui.rpcpool.com'
   # 'wss://rpc-mainnet.suiscan.xyz/websocket'
@@ -59,6 +45,20 @@ SUI_RPC_HOSTS=(
   # 'wss://sui-mainnet-rpc.bartestnet.com/websocket'
   # 'wss://sui-rpc-mainnet.testnet-pride.com/websocket'
   # 'wss://sui1mainnet-ws.chainode.tech'
+
+  # Testnet
+  # 'https://fullnode.testnet.sui.io'                 # new jersey
+  # 'https://rpc-testnet.suiscan.xyz'                 # california
+  # 'https://sui-testnet.nodeinfra.com'               # arizona
+  # 'https://fullnode.testnet.vincagame.com'          # germany
+  # 'https://sui-rpc.testnet.lgns.net'                # lithuania | obsolete data
+  # 'https://testnet.artifact.systems/sui'            # new jersey | The response returned from RPC server does not match the TypeScript definition. This is likely because the SDK version is not compatible with the RPC server.
+  # 'https://sui-testnet-rpc.allthatnode.com'         # germany | 429 when fetching profiles
+  # 'https://sui-testnet-rpc-germany.allthatnode.com' # germany | 429 when fetching profiles
+  # 'https://sui-testnet-endpoint.blockvision.org'    # singapore | 1.8s
+  # 'https://sui-testnet-rpc-korea.allthatnode.com'   # korea | 2.4s
+  # 'https://sui-testnet-fullnode.quantnode.tech'     # germany | Fast, but CORS error
+  # 'https://sui-rpc-pt.testnet-pride.com'            # france | 502
 )
 
 # Table header
