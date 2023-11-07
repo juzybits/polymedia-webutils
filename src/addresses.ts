121806f372b67726009cfa54a1e89cd6261606d6
juzybits <96890594+juzybits@users.noreply.github.com>
2023-11-07 00:55:46 -0100

    create shortenAddress()

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,3 +1,5 @@
 export const ADDRESS_REGEX = '0[xX][a-fA-F0-9]{64}';
 
-// TODO: add shortenAddress
+export function shortenAddress(address: string|undefined, start=4, end=4, prefix='', separator='..'): string {
+    return !address ? '' : prefix + address.slice(2, 2+start) + separator + address.slice(-end);
+}
