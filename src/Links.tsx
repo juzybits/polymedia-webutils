d32e090333724ff2fba8cc80ee7409b20df74aab
juzybits <96890594+juzybits@users.noreply.github.com>
2024-03-03 23:08:40 -0100

    LinkExternal to accept children

--- src/Links.tsx
+++ src/Links.tsx
@@ -5,20 +5,20 @@ import { NetworkName, makeSuiExplorerUrl, shortenSuiAddress } from '@polymedia/s
  * `<a target='_blank' rel='noopener noreferrer nofollow' href={href}>{text}</a>`
  */
 export const LinkExternal: React.FC<{
-    href: string;
-    text: string;
-    nofollow?: boolean;
+    href: string,
+    follow?: boolean,
+    children: React.ReactNode,
 }> = ({
     href,
-    text,
-    nofollow = true
+    follow = false,
+    children,
 }) => {
     return <a
         target='_blank'
-        rel={`noopener noreferrer ${nofollow ? 'nofollow' : ''}`}
+        rel={`noopener noreferrer ${follow ? '' : 'nofollow'}`}
         href={href}
     >
-        {text}
+        {children}
     </a>
 }
 
@@ -26,48 +26,45 @@ export const LinkExternal: React.FC<{
  * Create an external link to an address the Sui Explorer
  */
 export const LinkToExplorerAddr: React.FC<{
-    network: NetworkName;
-    addr: string;
+    network: NetworkName,
+    addr: string,
 }> = ({
     network,
     addr,
 }) => {
-    return <LinkExternal
-        href={makeSuiExplorerUrl(network, 'address', addr)}
-        text={shortenSuiAddress(addr)}
-    />;
+    return <LinkExternal href={makeSuiExplorerUrl(network, 'address', addr)}>
+        {shortenSuiAddress(addr)}
+    </LinkExternal>;
 }
 
 /**
  * Create an external link to an object the Sui Explorer
  */
 export const LinkToExplorerObj: React.FC<{
-    network: NetworkName;
-    objId: string;
+    network: NetworkName,
+    objId: string,
 }> = ({
     network,
     objId,
 }) => {
-    return <LinkExternal
-        href={makeSuiExplorerUrl(network, 'object', objId)}
-        text={shortenSuiAddress(objId)}
-    />;
+    return <LinkExternal href={makeSuiExplorerUrl(network, 'object', objId)}>
+        {shortenSuiAddress(objId)}
+    </LinkExternal>;
 }
 
 /**
  * Create an external link to a package the Sui Explorer
  */
 export const LinkToExplorerPkg: React.FC<{
-    network: NetworkName;
-    pkgId: string;
+    network: NetworkName,
+    pkgId: string,
 }> = ({
     network,
     pkgId,
 }) => {
-    return <LinkExternal
-        href={makeSuiExplorerUrl(network, 'package', pkgId)}
-        text={shortenSuiAddress(pkgId)}
-    />;
+    return <LinkExternal href={makeSuiExplorerUrl(network, 'package', pkgId)}>
+        {shortenSuiAddress(pkgId)}
+    </LinkExternal>;
 }
 
 
@@ -75,14 +72,13 @@ export const LinkToExplorerPkg: React.FC<{
  * Create an external link to a transaction block the Sui Explorer
  */
 export const LinkToExplorerTxn: React.FC<{
-    network: NetworkName;
-    txnId: string;
+    network: NetworkName,
+    txnId: string,
 }> = ({
     network,
     txnId,
 }) => {
-    return <LinkExternal
-        href={makeSuiExplorerUrl(network, 'txblock', txnId)}
-        text={shortenSuiAddress(txnId)}
-    />;
+    return <LinkExternal href={makeSuiExplorerUrl(network, 'txblock', txnId)}>
+        {shortenSuiAddress(txnId)}
+    </LinkExternal>;
 }
