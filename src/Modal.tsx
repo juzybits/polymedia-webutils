c1fd951baa7feda56a65cf3c22a1cfe2346912cb
juzybits <96890594+juzybits@users.noreply.github.com>
2024-04-09 12:59:18 +0000

    Modal: use styles/Modal.css

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,3 +1,5 @@
+import './Modal.css';
+
 export const Modal: React.FC<{
     content: React.ReactNode;
     styleBackground?: React.CSSProperties;
@@ -12,42 +14,11 @@ export const Modal: React.FC<{
         return null;
     }
 
-    const mergedBackgroundStyle = { ...defaultStyleBackground, ...styleBackground };
-    const mergedContentStyle = { ...defaultStyleContent, ...styleContent };
-
     return (
-        <div className='modal-background' style={mergedBackgroundStyle}>
-            <div className='modal-content' style={mergedContentStyle}>
+        <div className='modal-background' style={styleBackground}>
+            <div className='modal-content' style={styleContent}>
                 {content}
             </div>
         </div>
     );
 }
-
-const defaultStyleBackground: React.CSSProperties = {
-    position: 'fixed',
-    top: '0',
-    left: '0',
-    width: '100%',
-    height: '100lvh',
-    zIndex: '1000',
-    overflowY: 'auto',
-    padding: '1em',
-    boxSizing: 'border-box',
-    backgroundColor: 'rgba(0, 0, 0, 0.7)',
-};
-
-const defaultStyleContent: React.CSSProperties = {
-    position: 'relative',
-    margin: '0 auto',
-    width: 'min(800px, 100%)',
-    overflowWrap: 'break-word',
-    overflowY: 'scroll',
-    borderRadius: '10px',
-    padding: '1.7em 1.9em 2.1em',
-    boxSizing: 'border-box',
-    backgroundColor: 'rgb(255 255 255)',
-    fontSize: '1.2em',
-    textAlign: 'center',
-    color: 'black',
-};
